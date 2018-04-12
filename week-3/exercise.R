238f4d542837de829090fa193e78a38896b595b5
KJMEUA <kjmasumo@uw.edu>
2018-04-11 17:22:19 -0700

    finish lab ex w3

--- week-3/exercise.R
+++ week-3/exercise.R
@@ -1,16 +1,16 @@
 iris_df <- data.frame(iris)
 
 # Select all values of data frame for Sepal.length
-iris_df[, "Sepal.Length"]
+iris_df$Sepal.Length
 # Select only rows of Virginica flowers
-iris_df[iris_df$Species == "virginica", ]
+virginica <- iris_df[iris_df$Species == "virginica", ]
 # Select only the sepal legnth of the Virginica flowers
-dummy_df <- iris_df[iris_df$Species == "virginica", ]
-dummy_df["Sepal.Length"]
+virginica$Sepal.Length
 # Select rows where Petal.Length > 4.0
 iris_df[iris_df$Petal.Length > 4.0, ]
 # Add a column to the dataframe indicating whether Petal.Length > 4.0
 iris_df$long_petal <- iris_df$Petal.Length > 4.0
 # Find the species type of the flower that has the longest Sepal length
-
+iris_df[iris_df$Sepal.Length == max(iris_df$Sepal.Length), ]$Species
 # Select rows where species type is 'setosa' and Sepal.Width > 3.0
+iris_df[(iris_df$Species == "setosa") & (iris_df$Sepal.Width > 3.0), ]
